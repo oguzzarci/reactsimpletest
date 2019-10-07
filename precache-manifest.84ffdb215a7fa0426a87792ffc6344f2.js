fb6456c8c4fffe87ee1cedb3326120283cca601e
oguzzarci <oguz.zarci@digiturk.com.tr>
2019-10-07 09:21:51 +0300

    Updates

---
+++ precache-manifest.84ffdb215a7fa0426a87792ffc6344f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a397bf59ad28ab8fa0cdd9c00a2f0ce",
+    "url": "/reactsimpletest/index.html"
+  },
+  {
+    "revision": "c43db8653cf96177a863",
+    "url": "/reactsimpletest/static/css/2.7c4efcaa.chunk.css"
+  },
+  {
+    "revision": "1f40a50d008adc0e5dfa",
+    "url": "/reactsimpletest/static/css/main.b5623824.chunk.css"
+  },
+  {
+    "revision": "c43db8653cf96177a863",
+    "url": "/reactsimpletest/static/js/2.74edac93.chunk.js"
+  },
+  {
+    "revision": "1f40a50d008adc0e5dfa",
+    "url": "/reactsimpletest/static/js/main.6b787482.chunk.js"
+  },
+  {
+    "revision": "c012cdc9f6b3bf0887b2",
+    "url": "/reactsimpletest/static/js/runtime-main.f8243d16.js"
+  },
+  {
+    "revision": "2522bc42d15c6f50ae6ba88de69b2b1e",
+    "url": "/reactsimpletest/static/media/alogo.2522bc42.png"
+  },
+  {
+    "revision": "d18071ac3b75e9f2b10b229b8739febf",
+    "url": "/reactsimpletest/static/media/apple.d18071ac.png"
+  },
+  {
+    "revision": "ffe430d7d718267498138a96ad52115e",
+    "url": "/reactsimpletest/static/media/sdp.ffe430d7.jpg"
+  }
+]);
